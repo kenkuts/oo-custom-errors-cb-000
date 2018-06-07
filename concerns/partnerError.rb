37dc687d06768f7a3993a146026932d825ab3de2
kenkuts <kennethchoi19@gmail.com>
2018-06-07 16:16:54 -0400

    finished flatiron codealong for creating custom errors.

---
+++ concerns/partnerError.rb
@@ -0,0 +1,3 @@
+module PartnerError < StandardError
+
+end
--- custom_errors.rb
+++ custom_errors.rb
@@ -1,3 +1,4 @@
+
 class Person
   attr_accessor :partner, :name
 
@@ -7,15 +8,26 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin                         # A rescue error allows our program to run after it has encountered the error
+        raise PartnerError          # it will show a PartnerError an will print out a message the go back and
+      rescue PartnerError => error  # execute the program.
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
-
 end
+
+class PartnerError < StandardError
+  def message
+    "you must give the get_married method an
+     argument of an instance of the person class!"
+  end
+end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
